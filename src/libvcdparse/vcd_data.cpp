bc8f528a429a4ec03068047cbc690bfdfa37e6f6
kmurray <k.murray@utoronto.ca>
2016-04-27 19:01:32 -0400

    Be consistent with modelsim literal cases

--- src/libvcdparse/vcd_data.cpp
+++ src/libvcdparse/vcd_data.cpp
@@ -5,16 +5,16 @@ namespace vcdparse {
     std::ostream& operator<<(std::ostream& os, LogicValue val) {
         if(val == LogicValue::ONE) os << "1";
         else if(val == LogicValue::ZERO) os << "0";
-        else if(val == LogicValue::UNKOWN) os << "X";
-        else if(val == LogicValue::HIGHZ) os << "Z";
+        else if(val == LogicValue::UNKOWN) os << "x";
+        else if(val == LogicValue::HIGHZ) os << "z";
         else assert(false);
         return os;
     }
 
     std::ostream& operator<<(std::ostream& os, Var::Type type) {
-        if(type == Var::Type::WIRE) os << "WIRE";
-        else if(type == Var::Type::REG) os << "REG";
-        else if(type == Var::Type::PARAMETER) os << "PARAMETER";
+        if(type == Var::Type::WIRE) os << "wire";
+        else if(type == Var::Type::REG) os << "reg";
+        else if(type == Var::Type::PARAMETER) os << "parameter";
         else assert(false);
         return os;
     }
