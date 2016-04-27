a6df8fee5dfd2a58024ef04b7659d3b4739374a1
kmurray <k.murray@utoronto.ca>
2016-04-27 19:01:12 -0400

    Add better printing support.

--- src/vcdparse_demo/main.cpp
+++ src/vcdparse_demo/main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include <map>
 
 #include "vcdparse.hpp"
 
 using namespace vcdparse;
 
 void print_vcd(const VcdData& vcd_data);
+void print_header(const Header& header);
+void print_vars(const VcdData& vcd_data);
+void print_changes(const VcdData& vcd_data);
 
 int main(int argc, char** argv) {
 
@@ -19,7 +23,7 @@ int main(int argc, char** argv) {
         std::cout << "Successfully loaded VCD\n";
 
         auto& vcd_data = vcd_loader.get_vcd_data();
-        print_vcd(vcd_data);
+        //print_vcd(vcd_data);
         return 0;
     } else {
         std::cout << "Failed to load VCD\n";
@@ -28,15 +32,57 @@ int main(int argc, char** argv) {
 }
 
 void print_vcd(const VcdData& vcd_data) {
-    for(auto& signal_values : vcd_data.signal_values()) {
-        auto& var = signal_values.var();
-        std::cout << "Var: " << var.name() << " (" << var.id() << ")\n";
+    print_header(vcd_data.header());
+    print_vars(vcd_data);
+    print_changes(vcd_data);
+}
 
-        auto& tvs = signal_values.time_values();
-        size_t len = 1000;
-        len = std::min(len, tvs.size());
-        for(size_t i = 0; i < len; ++i) {
-            std::cout << "\tTime: " << tvs[i].time() << " Values: " << tvs[i].value() << "\n";
+void print_header(const Header& header) {
+    std::cout << "$date\n";
+    std::cout << header.date() << "\n";
+    std::cout << "$end\n";
+    std::cout << "$version\n";
+    std::cout << header.version() << "\n";
+    std::cout << "$end\n";
+    std::cout << "$timescale\n";
+    std::cout << header.timescale() << "\n";
+    std::cout << "$end\n";
+}
+void print_vars(const VcdData& vcd_data) {
+    for(const auto& sigvals : vcd_data.signal_values()) {
+        const auto& var = sigvals.var();
+        std::cout << "$var ";
+        std::cout << var.type() << " ";
+        std::cout << var.width() << " ";
+        std::cout << var.id() << " ";
+        std::cout << var.name() << " ";
+        std::cout << "$end\n";
+    }
+    std::cout << "$enddefinitions $end\n";
+}
+
+void print_changes(const VcdData& vcd_data) {
+    //Collect all the changes by time
+    std::map<size_t,std::vector<std::tuple<LogicValue,std::string>>> changes_by_time;
+    for(const auto& sigvals : vcd_data.signal_values()) {
+        const auto& var = sigvals.var();
+        for(const auto& tv : sigvals.time_values()) {
+            changes_by_time[tv.time()].push_back(std::make_tuple(tv.value(), var.id()));
         }
     }
+
+    //Print them out
+    for(auto kv : changes_by_time) {
+        size_t time = kv.first;
+
+        std::cout << "#" << time << "\n";
+
+        for(auto change : kv.second) {
+            LogicValue val;
+            std::string id;
+            std::tie(val, id) = change;
+            std::cout << val << id << "\n";
+        }
+    }
+
 }
